8c084dcb07008ade31881053502fc95a5914d749
Yamit <dreiko200590@hotmail.com>
2022-11-02 11:59:36 +0200

    the eamil implementation with its variables

--- sending_email.py
+++ sending_email.py
@@ -3,3 +3,14 @@ import ssl
 from email.message import EmailMessage
 
 
+subject  = "Email from the project bypython"
+
+body = " Test email fromthe aplication that wqe are trying to build for creating from Python!"
+sender_email =  "yaveloper@gmail.com"
+receiver_email =  "dreiko200590@hotmail.com"
+password = input("Please enter the password: ")
+
+message = EmailMessage()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
