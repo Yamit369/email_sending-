0b17b1029e2478dc43c599186712924446be0f99
Yamit <dreiko200590@hotmail.com>
2022-11-02 12:09:12 +0200

    implemattions of ssl and conection to the server

--- sending_email.py
+++ sending_email.py
@@ -14,3 +14,14 @@ message = EmailMessage()
 message["From"] = sender_email
 message["To"] = receiver_email
 message["Subject"] = subject
+message.set_content(body)
+
+context = ssl.create_default_context()
+
+print("Sending email...")
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as  server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email,receiver_email, message.as_string)
+
+print(" Succes. Email sent!")
